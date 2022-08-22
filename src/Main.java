7a39ee6332f08c85c0da89b7dc72cdd9778ae30a
Lenovo <nadyasafarova98@gmail.com>
2022-08-22 14:30:01 +0300

    ДЗ от Нади 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
-}
+
+    //Задание1
+    public static void task1() {
+        {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+
+            String fullName = lastName + " " + firstName + " " + middleName;
+            System.out.println("ФИО сотрудника — " + fullName);
+        }
+    }
+
+    //Задание 2
+    public static void task2() {
+        {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+
+            String fullName = lastName + " " + firstName + " " + middleName;
+            var result = fullName.toUpperCase();
+            System.out.println("Данные ФИО сотрудника для заполнения отчета — " + result);
+        }
+    }
+
+    //Задание 3
+    public static void task3() {
+        {
+            var fullName = "Иванов Семён Семёнович";
+            var result = fullName.replace("ё", "е");
+            System.out.printf("Данные ФИО сотрудника — %s", result);
+        }        }
+
+    }
